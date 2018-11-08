18ac4e15cd7be914b3699aaa16871ade709d123a
chris brown <chris@digitalbrains.io>
2018-11-07 16:05:17 -0800

    adding css grid

--- gulpfile.js
+++ gulpfile.js
@@ -1,103 +1,151 @@
-var browserSync = require('browser-sync').create(),
-    changed     = require('gulp-changed'),
-    config      = require('./config.json').configuration,
-    gulp        = require('gulp'),
-    gulputil    = require('gulp-util'),
-    gulpwatch   = require('gulp-watch'),
-    reload      = browserSync.reload,
-    sass        = require('gulp-sass'),
+/**
+ * - - - - - - - - - - EDIT YOUR PATHS
+ * - - - - - - - - - - - - - - - - - -
+ */
+let paths = {
+        styles: {
+            src: 'src/scss/**/*.scss',
+            dest: 'css/'
+        },
+        scripts: {
+            src: 'src/javascript/**/*.js',
+            dest: 'javascript/'
+        },
+        url: {
+            dev: 'http://ladonnawitmer'
+        }
+    };
+
+
+
+
+
+
+
+
+
+
+/**
+ * - - - - - - - - - - INCLUDE YOUR MODULES
+ * - - - - - - - - - - - - - - - - - - - - -
+ */
+
+let prefix      = require ('gulp-autoprefixer'),
+    beeper      = require ('beeper'),
+    browserSync = require ('browser-sync').create (),
+    //changed     = require ('gulp-changed'),
+    colors      = require ('ansi-colors'),
+    gulp        = require ('gulp'),
+    log         = require ('fancy-log'),
+    plumber     = require ('gulp-plumber'),
+    reload      = browserSync.reload ,
+    sass        = require ('gulp-sass'),
+    sourcemaps  = require ('gulp-sourcemaps'),
     uglify      = require('gulp-uglify');
 
-/**
- * Style
- */
-gulp.task('style', function () {
-    return gulp.src(config.source.style)
-        .pipe(changed(config.destination.style, {extension: '.css'}))
-        .pipe(sass({'outputStyle': 'compressed'}))
-        .pipe(gulp.dest(config.destination.style))
-        .pipe(reload({stream:true}));
-});
+
+
+
+
+
+
+
+
 
 /**
- * Images
+ * - - - - - - - - - - FUNCTIONS
+ * - - - - - - - - - - - - - - -
  */
-gulp.task('image', function () {
-    return gulp.src(config.source.img)
-        .pipe(changed(config.destination.img))
-        .pipe(gulp.dest(config.destination.img))
-        .pipe(reload({stream:true}));
-});
 
-/**
- * Script
- */
-gulp.task('script', function () {
-    return gulp.src(config.source.js)
-        .pipe(changed(config.destination.js, {extension: '.js'}))
+// Log errors and don't break the stream.
+function onError(err) {
+    console.log(err);
+    beeper(2);
+};
+
+// Create a new proxy server to view dest.
+function server() {
+    browserSync.init ({
+       proxy  : paths.url.dev,
+       notify : false
+    });
+    log(colors.redBright.bold.bgWhiteBright("Excelsior! We're up and running."));
+};
+
+// Compile scss into css and reload proxy server.
+// 'production' mode will compress final css output.
+function styles(env) {
+    if (env !== true) {
+        return gulp.src(paths.styles.src)
+        .pipe(sourcemaps.init())
+        .pipe(sass({'outputStyle':'expanded'}))
+        .pipe(plumber({
+              errorHandler : onError
+          }))
+        .pipe(prefix({
+            grid: true,
+            flexbox: true
+        }))
+        .pipe(sourcemaps.write('./')) //maps are set relative to source
+        .pipe(gulp.dest(paths.styles.dest))
+        .pipe(browserSync.stream());
+    } else {
+        return gulp.src(paths.styles.src)
+        .pipe(sourcemaps.init())
+        .pipe(sass({'outputStyle':'compressed'}))
+        .pipe(sourcemaps.write('./')) //maps are set relative to source
+        .pipe(gulp.dest(paths.styles.dest));
+    }
+}
+
+// Save JavaScript to dest and reload browser.
+// 'production' task will uglify final JavaScript output.
+function scripts(env) {
+    if (env !== true) {
+        return gulp.src(paths.scripts.src)
+        .pipe(plumber({
+              errorHandler : onError
+          }))
+        .pipe(gulp.dest(paths.scripts.dest))
+        .pipe(browserSync.stream());
+    } else {
+        return gulp.src(paths.scripts.src)
         .pipe(uglify())
-        .pipe(gulp.dest(config.destination.js))
-        .pipe(reload({stream:true}));
-});
+        .pipe(gulp.dest(paths.scripts.dest));
+    }
+}
+
+// Watching
+function watch() {
+    // watch source files
+    gulp.watch(paths.scripts.src, scripts);
+    gulp.watch(paths.styles.src, styles);
+}
+
+
+
+
+
+
+
+
+
 
 /**
- * HTML
+ * - - - - - - - - - - TASKS
+ * - - - - - - - - - - - - -
  */
-gulp.task('html', function () {
-    return gulp.src(config.source.html)
-        .pipe(changed(config.destination.html, {extension: '.html'}))
-        .pipe(gulp.dest(config.destination.html))
-        .pipe(reload({stream:true}));
+
+// Render production ready assets
+gulp.task('production', function(done) {
+    styles(true);
+    scripts(true);
+    done();
 });
 
-/**
- * Server
- */
-gulp.task('server', function (){
-    browserSync.init({
-        proxy: config.devUrl
-    });
-});
-
-/**
- * Watch
- */
-gulp.task('watch', function (){
-
-    gulpwatch(config.source.style, ['style'], function () {
-        gulp.src(config.source.style)
-        .pipe(changed(config.destination.style, {extension: '.css'}))
-        .pipe(sass({'outputStyle': 'compressed'}))
-        .pipe(gulp.dest(config.destination.style))
-        .pipe(reload({stream:true}));
-    });
-
-    gulpwatch(config.source.img, ['image'], function () {
-        gulp.src(config.source.img)
-        .pipe(changed(config.destination.img))
-        .pipe(gulp.dest(config.destination.img))
-        .pipe(reload({stream:true}));
-    });
-
-    gulpwatch(config.source.js, ['script'], function () {
-        gulp.src(config.source.js)
-        .pipe(changed(config.destination.js, {extension: '.js'}))
-        .pipe(uglify())
-        .pipe(gulp.dest(config.destination.js))
-        .pipe(reload({stream:true}));
-    });
-
-    gulpwatch(config.source.html, ['html'], function () {
-        gulp.src(config.source.html)
-        .pipe(changed(config.destination.html, {extension: '.html'}))
-        .pipe(gulp.dest(config.destination.html))
-        .pipe(reload({stream:true}));
-    });
-});
-
-/**
- * Default task
- */
-gulp.task('default', ['style', 'image', 'script', 'html', 'server', 'watch'], function () {
-    gulputil.log(gulputil.colors.inverse("Excelsior! We're up and running."));
+gulp.task('default', function() {
+    styles();
+    scripts();
+    server();
+    watch();
 });
