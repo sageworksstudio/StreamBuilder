a23a9188e94d0409cd52c2bab86c9aa667aa476a
chris brown <chris@sageworksstudio.com>
2020-01-20 08:20:12 -0800

    dist

---
+++ dist/js/app.js
@@ -0,0 +1,40 @@
+// Custom functions
+(function () {
+  'use strict'
+
+  // Page specific actions
+  const MYSITE = {
+    common: { // sitewide code
+      init: function () {
+        // Do common stuff here
+      }
+    },
+
+    Page: {
+      init: function () { // controller-wide code
+        // Do Page specific stuff here
+      }
+    }
+  }
+
+  const UTIL = {
+    exec: function (controller, action) {
+      const namespace = MYSITE
+      action = (action === undefined) ? 'init' : action
+      if (controller !== '' && namespace[controller] && typeof namespace[controller][action] === 'function') {
+        namespace[controller][action]()
+      }
+    },
+
+    init: function () {
+      const elm = document.getElementById('main')
+      const controller = elm.getAttribute('data-controller')
+      const action = elm.getAttribute('data-action')
+      UTIL.exec('common')
+      UTIL.exec(controller)
+      UTIL.exec(controller, action)
+    }
+  }
+
+  window.onload = UTIL.init
+})()
